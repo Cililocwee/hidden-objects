37382995b3ac07355c16576eccf5cfba59062b00
Cililocwee <corrie.stroup@gmail.com>
2023-01-01 03:48:08 +0700

    prune unused code

--- src/components/SelectionCard.jsx
+++ src/components/SelectionCard.jsx
@@ -22,16 +22,15 @@ export default function SelectionCard({ cardX, cardY, escapeHatch }) {
   // TODO Boxes don't need to persist and no longer have to to have individual cards keep track
   // TODO of animal selection
   function handleClick(choice) {
-    // ** should take in the choice as a key and add an arr with coords
-    manipulateZoo([choice], [cardX, cardY]);
+    // should take in the choice as a key and add an arr with coords
+    // manipulateZoo([choice], [cardX, cardY]);
 
-    // shows which animal was selected
-    setAnimalChoice(choice);
-    // removes animal from the selection list
-    changeAnimalList(choice, "remove");
+    // sets state of animalChoice
+    // setAnimalChoice(choice);
 
-    // TODO beginning of refactor
-    checkAnswer(choice);
+    if (checkAnswer(choice, [cardX, cardY])) {
+      changeAnimalList(choice, "remove");
+    }
   }
 
   // !! This should only be for debugging, in final release, this should be unused
@@ -40,20 +39,22 @@ export default function SelectionCard({ cardX, cardY, escapeHatch }) {
     escapeHatch([cardX, cardY]);
   }
 
-  function checkAnswer(choice) {
+  function checkAnswer(choice, arr) {
     const answerObj = convertArrOfObjsToObj(answerKey);
-    // console.log(choice);
-    // console.log([cardX, cardY]);
-    // console.log(answerKey);
-    // console.log(answerObj);
-    // console.log(answerObj[choice]);
-    // console.log(zoo);
 
-    let xDiff = answerObj[choice][0] - zoo[choice][0];
-    let yDiff = answerObj[choice][1] - zoo[choice][1];
-    console.log(Math.sqrt(xDiff * xDiff + yDiff * yDiff));
+    let xDiff = answerObj[choice][0] - arr[0];
+    let yDiff = answerObj[choice][1] - arr[1];
+    const answerDiff = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+
+    // if selection is within 30px
+    if (answerDiff < 30) {
+      console.log("CLOSE!");
+      return true;
+    }
   }
 
+  // TODO all of this needs to eventually go
+  // TODO boxes shouldn't stay up
   if (!animalChoice) {
     return (
       <div
